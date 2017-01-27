8536e9522ee8893adc7a00e5b7915a4d0e1f35d9
marivasi5 <mariavasilarou@gmail.com>
2017-01-27 22:41:53 +0200

    Add files via upload

--- freq.py
+++ freq.py
@@ -36,55 +36,63 @@ for line in f:
 #==============================================================================
 f.close()    
 
-with open('/home/rantaplan/master/togamatoproject/data/mikrakicases.txt') as cases, open('/home/rantaplan/master/togamatoproject/data/mikrakicontrols.txt') as controls: 
-    for line_cases, line_controls in zip(cases, controls):
-        splittedline_cases= line_cases.split(' ')   #einai lista, ta items tis anagnwrizontai ws strings
-        splittedline_controls= line_controls.split(' ')          
-        snp= splittedline_cases[0]
-        
-        #STA CASES
-        CASEShomozygous_refrence=0
-        CASEShomozygous_alternative=0
-        CASESheterozygous=0
+# x= /home/rantaplan/master/togamatoproject/data/mikrakicases.txt
+#allele_frequencies('/home/rantaplan/master/togamatoproject/data/mikrakicases.txt', '/home/rantaplan/master/togamatoproject/data/mikrakicontrols.txt'
 
-        for i in range(5, len(splittedline_cases), 3): #!tsekare to range
-            individual= splittedline_cases[i] + splittedline_cases[i+1] + splittedline_cases[i+2]
-            if individual=='100':
-                CASEShomozygous_refrence+=1
-            elif individual=='010':
-                CASESheterozygous+=1
-            elif individual=='001':
-                CASEShomozygous_alternative+=1
+#SOS elegxos antistixisis tou x me tta cases kai tou y me ta control (kai oxi antistrofa)
+def allele_frequencies(x,y,z):
+    '''grapse ti kanei  x:cases y;Controls, z; onoma output'''
+    
+    output_name=open(z, 'w')
+    with open(x) as cases, open(y) as controls: 
+        for line_cases, line_controls in zip(cases, controls):
+            splittedline_cases= line_cases.split(' ')   #einai lista, ta items tis anagnwrizontai ws strings
+            splittedline_controls= line_controls.split(' ')          
+            snp= splittedline_cases[0]
             
-                 #ypologismos suxnotitwn:
-            pCASES= (2*CASEShomozygous_refrence + CASESheterozygous)/1000  #einai /2N opou N=500   
-            qCASES= 1-pCASES
-        
-        #STA CONTROLS
-        CONTROLShomozygous_refrence=0
-        CONTROLShomozygous_alternative=0
-        CONTROLSheterozygous=0
-
-        for i in range(5, len(splittedline_controls), 3): #!tsekare to range
-            individual= splittedline_controls[i] + splittedline_controls[i+1] + splittedline_controls[i+2]
-            if individual=='100':
-                CONTROLShomozygous_refrence+=1
-            elif individual=='010':
-                CONTROLSheterozygous+=1
-            elif individual=='001':
-                CONTROLShomozygous_alternative+=1
+            #STA CASES
+            CASEShomozygous_refrence=0
+            CASEShomozygous_alternative=0
+            CASESheterozygous=0
+    
+            for i in range(5, len(splittedline_cases), 3): #!tsekare to range
+                individual= splittedline_cases[i] + splittedline_cases[i+1] + splittedline_cases[i+2]
+                if individual=='100':
+                    CASEShomozygous_refrence+=1
+                elif individual=='010':
+                    CASESheterozygous+=1
+                elif individual=='001':
+                    CASEShomozygous_alternative+=1
+                
+                     #ypologismos suxnotitwn:
+                pCASES= (2*CASEShomozygous_refrence + CASESheterozygous)/1000  #einai /2N opou N=500   
+                qCASES= 1-pCASES
             
-                 #ypologismos suxnotitwn:
-            pCONTROLS= (2*CONTROLShomozygous_refrence + CONTROLSheterozygous)/1000  #einai /2N opou N=500   
-            qCONTROLS= 1-pCONTROLS
-        
-        #KOITAKSE TO EDIT
-        print(snp, round(pCONTROLS, 3) ,'\t', round(qCONTROLS, 3) ,'\t', round(pCASES,3) ,'\t', round(qCASES,3))
-        
-        
-        
-        
-        
+            #STA CONTROLS
+            CONTROLShomozygous_refrence=0
+            CONTROLShomozygous_alternative=0
+            CONTROLSheterozygous=0
+    
+            for i in range(5, len(splittedline_controls), 3): #!tsekare to range
+                individual= splittedline_controls[i] + splittedline_controls[i+1] + splittedline_controls[i+2]
+                if individual=='100':
+                    CONTROLShomozygous_refrence+=1
+                elif individual=='010':
+                    CONTROLSheterozygous+=1
+                elif individual=='001':
+                    CONTROLShomozygous_alternative+=1
+                
+                     #ypologismos suxnotitwn:
+                pCONTROLS= (2*CONTROLShomozygous_refrence + CONTROLSheterozygous)/1000  #einai /2N opou N=500   
+                qCONTROLS= 1-pCONTROLS
+            
+            #KOITAKSE TO EDIT # kanto me return #PREPEI NA GRAFEI SE ARXEIO
+            print(snp, round(pCONTROLS, 3) ,'\t', round(qCONTROLS, 3) ,'\t', round(pCASES,3) ,'\t', round(qCASES,3), '\t', round(pCASES+pCONTROLS ,3), '\t', round(qCONTROLS+qCONTROLS,3), file=output_name)
+            output_name.close()
+            
+            
+            
+            
         
         
         
