356ed85707a8ebff30332b377f120111bb0ac683
marivasi5 <mariavasilarou@gmail.com>
2017-02-04 11:01:02 +0200

    Update pinakasdipliseisodou.py

--- pinakasdipliseisodou.py
+++ pinakasdipliseisodou.py
@@ -42,7 +42,40 @@ for i in range(1, len(snpA_splitted) -len(snpA_splitted)%3 ,3):
             Ah+=1
         elif b_individual == '001':
             AA+=1
-            
+ 
+#%%            
+def genotype_counts(datasetLINE):    #prepei na to taiso lines    
+    splittedline= datasetLINE.split(' ')   #einai lista, ta items tis anagnwrizontai ws strings
+    
+    snp= splittedline[0]
+    homozygous_refrence=0
+    homozygous_alternative=0
+    heterozygous=0
+    for i in range(5, len(splittedline) -len(splittedline)%3, 3): #!tsekare to range
+                individual= splittedline[i] + splittedline[i+1] + splittedline[i+2]
+                if individual=='100':
+                    homozygous_refrence+=1
+                elif individual=='010':
+                    heterozygous+=1
+                elif individual=='001':
+                    homozygous_alternative+=1
+    return snp , homozygous_refrence, homozygous_alternative, heterozygous 
+    #+' '+  str(homozygous_alternative) +' '+  str(homozygous_alternative) 
+    #me tabs eixe thema        
+#%%
+def allele_freq(datasetLINE):
+    #gia upologismo atomwn dataset (kathe grammi exei ton idio arithmo atomwn me tis alles vevaia)
+    splittedline= datasetLINE.split(' ') #ena line twn 500: exei len 1505(=3*N+5)
+    N=(len(splittedline)-5)/3
+    
+    p= round((genotype_counts(datasetLINE)[1]*2 + genotype_counts(datasetLINE)[3])/(2*N), 3)  #einai /2N opou N=500   
+    q= round((genotype_counts(datasetLINE)[2]*2 + genotype_counts(datasetLINE)[3])/(2*N), 3)
+    
+    return splittedline[0], p, q
+#%%     gia na vrw ta pA pB
+alleles=list(map(allele_freq, snps))
+pA=alleles[0][1]           
+pB=alleles[1][1]           
             
             
             
